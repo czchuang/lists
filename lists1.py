a48eb4a17a39fabbf6813b8f8d814aeec3ca9cd5
sandi <sandimac@users.noreply.github.com>
2016-02-02 19:50:22 -0800

    add python file

---
+++ lists1.py
@@ -0,0 +1,11 @@
+my_list = [1, 2, "kitten", 4, "five"]
+my_list[2]
+my_list[3]
+my_list[-1]
+my_list[4]
+print my_list[len(my_list)-1]
+len(my_list)
+
+my_list = [1, 2, "kitten", 4, "five", 6, 7]
+print my_list[1:10]
+print my_list
